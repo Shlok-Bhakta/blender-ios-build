5adece6c006c89e81124da78f669a035161f5760
Shlok Bhakta <shlokbhakta1@gmail.com>
2026-03-26 00:14:46 -0500

    Iter 62: Disable OpenSubdiv for iOS builds

--- build_files/cmake/platform/platform_apple.cmake
+++ build_files/cmake/platform/platform_apple.cmake
@@ -36,6 +36,7 @@ if(WITH_APPLE_CROSSPLATFORM)
   set(WITH_SDL OFF CACHE BOOL ${NO_PLATFORM_SUPPORT_MSG} FORCE)
   set(WITH_INPUT_NDOF OFF CACHE BOOL ${NO_PLATFORM_SUPPORT_MSG} FORCE)
   set(WITH_PYTHON_MODULE OFF CACHE BOOL ${NO_PLATFORM_SUPPORT_MSG} FORCE)
+  set(WITH_OPENSUBDIV OFF CACHE BOOL "Auto disabled because OpenSubdiv GPU evaluator depends on OpenGL/Epoxy" FORCE)
   # Disable these modules for now
   set(WITH_PYTHON_INSTALL_ZSTANDARD OFF CACHE BOOL "Disable until iOS build supports SSL" FORCE)
   # Disable Audaspace as it had dependencies on CoreAudio components which do not exist on iOS
