9cda7240a5611c49a82e8bd3a9844026257f67ad
Shlok Bhakta <shlokbhakta1@gmail.com>
2026-03-29 21:22:55 -0500

    MINIMAX TAKEOVER: Patch brotli CMakeLists.txt to disable CLI tool install

--- build_files/build_environment/cmake/brotli.cmake
+++ build_files/build_environment/cmake/brotli.cmake
@@ -2,12 +2,13 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-set(BROTLI_EXTRA_ARGS
-  -DCMAKE_OSX_BUNDLE=OFF
-)
-
+set(BROTLI_PATCH_COMMAND)
 if(APPLE AND WITH_APPLE_CROSSPLATFORM)
-  list(APPEND BROTLI_EXTRA_ARGS -DCMAKE_POLICY_VERSION_MINIMUM=3.5)
+  set(BROTLI_PATCH_COMMAND
+    COMMAND ${CMAKE_COMMAND} -E sed -i.bak
+      "s/install(TARGETS brotli RUNTIME DESTINATION \"\\${CMAKE_INSTALL_BINDIR}\")/# DISABLED for iOS cross-compile: install(TARGETS brotli RUNTIME DESTINATION \"\\${CMAKE_INSTALL_BINDIR}\")/"
+      ${BUILD_DIR}/brotli/src/external_brotli/CMakeLists.txt
+  )
 endif()
 
 ExternalProject_Add(external_brotli
@@ -16,11 +17,12 @@ ExternalProject_Add(external_brotli
   URL_HASH ${BROTLI_HASH_TYPE}=${BROTLI_HASH}
   PREFIX ${BUILD_DIR}/brotli
   CMAKE_GENERATOR ${PLATFORM_ALT_GENERATOR}
+  PATCH_COMMAND ${BROTLI_PATCH_COMMAND}
 
   CMAKE_ARGS
     -DCMAKE_INSTALL_PREFIX=${LIBDIR}/brotli
     ${DEFAULT_CMAKE_FLAGS}
-    ${BROTLI_EXTRA_ARGS}
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 
   INSTALL_DIR ${LIBDIR}/brotli
 )
