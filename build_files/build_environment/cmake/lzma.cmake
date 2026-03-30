850ddef6267f6c0384506be4a0b1782207b9461f
Shlok Bhakta <shlokbhakta1@gmail.com>
2026-03-29 22:54:52 -0500

    MINIMAX TAKEOVER: Add PLATFORM_HOST_TARGET for iOS cross-compilation
    Autotools configure scripts need --host flag for cross-compilation.
    Without --host, configure tries to run compiled programs on the build
    machine, which fails for iOS cross-compilation.

--- build_files/build_environment/cmake/lzma.cmake
+++ build_files/build_environment/cmake/lzma.cmake
@@ -13,7 +13,7 @@ ExternalProject_Add(external_lzma
 
   CONFIGURE_COMMAND ${CONFIGURE_ENV} &&
     cd ${BUILD_DIR}/lzma/src/external_lzma/ &&
-    ${CONFIGURE_COMMAND} --prefix=${LIBDIR}/lzma --disable-shared
+    ${CONFIGURE_COMMAND} --prefix=${LIBDIR}/lzma --disable-shared ${PLATFORM_HOST_TARGET}
 
   BUILD_COMMAND ${CONFIGURE_ENV} &&
     cd ${BUILD_DIR}/lzma/src/external_lzma/ &&
