bba23506a18dc5731658fe4b63f89793a4947e5a
Shlok Bhakta <shlokbhakta1@gmail.com>
2026-03-29 21:38:41 -0500

    MINIMAX TAKEOVER: Fix brotli - skip CLI tool via BROTLI_CLI_C= and patch install step

--- build_files/build_environment/cmake/brotli.cmake
+++ build_files/build_environment/cmake/brotli.cmake
@@ -3,11 +3,14 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 if(APPLE AND WITH_APPLE_CROSSPLATFORM)
-  set(BROTLI_PATCH_COMMAND COMMAND /usr/bin/perl -i -0pe
-    's/install\(\s*\n\s*TARGETS brotli\s*\n\s*RUNTIME DESTINATION[^)]*\)/# PATCHED for iOS: DISABLED brotli CLI tool install/s'
-    ${BUILD_DIR}/brotli/src/external_brotli/CMakeLists.txt)
+  set(BROTLI_EXTRA_ARGS
+    -DBROTLI_CLI_C=
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+  )
 else()
-  set(BROTLI_PATCH_COMMAND)
+  set(BROTLI_EXTRA_ARGS
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+  )
 endif()
 
 ExternalProject_Add(external_brotli
@@ -16,15 +19,24 @@ ExternalProject_Add(external_brotli
   URL_HASH ${BROTLI_HASH_TYPE}=${BROTLI_HASH}
   PREFIX ${BUILD_DIR}/brotli
   CMAKE_GENERATOR ${PLATFORM_ALT_GENERATOR}
-  PATCH_COMMAND ${BROTLI_PATCH_COMMAND}
 
   CMAKE_ARGS
     -DCMAKE_INSTALL_PREFIX=${LIBDIR}/brotli
     ${DEFAULT_CMAKE_FLAGS}
-    -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+    ${BROTLI_EXTRA_ARGS}
 
   INSTALL_DIR ${LIBDIR}/brotli
 )
+
+if(APPLE AND WITH_APPLE_CROSSPLATFORM)
+  ExternalProject_Add_Step(external_brotli patch_brotli
+    COMMAND perl -i -0pe
+      's/install\(\s*\n\s*TARGETS brotli\s*\n\s*RUNTIME DESTINATION[^)]*\)/# PATCHED for iOS cross-compile disabled brotli CLI install/sg'
+      ${BUILD_DIR}/brotli/src/external_brotli/CMakeLists.txt
+    DEPENDEES download
+    DEPENDERS configure
+  )
+endif()
 
 if(WIN32)
   if(BUILD_MODE STREQUAL Release)
