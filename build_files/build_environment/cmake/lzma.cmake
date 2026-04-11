8aefe1295b243fc8c38dd6e18bdce285de81a83c
Shlok Bhakta <shlokbhakta1@gmail.com>
2026-04-11 02:07:23 -0500

    Declare LZMA cross host for Apple deps

--- build_files/build_environment/cmake/lzma.cmake
+++ build_files/build_environment/cmake/lzma.cmake
@@ -4,6 +4,16 @@
 
 set(LZMA_PATCH_CMD echo .)
 
+if(WITH_APPLE_CROSSPLATFORM)
+  if("${CMAKE_OSX_ARCHITECTURES}" STREQUAL "x86_64")
+    set(LZMA_EXTRA_ARGS --host=x86_64-apple-darwin19.0.0)
+  else()
+    set(LZMA_EXTRA_ARGS --host=aarch64-apple-darwin20.0.0)
+  endif()
+else()
+  set(LZMA_EXTRA_ARGS)
+endif()
+
 ExternalProject_Add(external_lzma
   URL file://${PACKAGE_DIR}/${LZMA_FILE}
   DOWNLOAD_DIR ${DOWNLOAD_DIR}
@@ -13,7 +23,7 @@ ExternalProject_Add(external_lzma
 
   CONFIGURE_COMMAND ${CONFIGURE_ENV} &&
     cd ${BUILD_DIR}/lzma/src/external_lzma/ &&
-    ${CONFIGURE_COMMAND} --prefix=${LIBDIR}/lzma --disable-shared
+    ${CONFIGURE_COMMAND} --prefix=${LIBDIR}/lzma --disable-shared ${LZMA_EXTRA_ARGS}
 
   BUILD_COMMAND ${CONFIGURE_ENV} &&
     cd ${BUILD_DIR}/lzma/src/external_lzma/ &&
