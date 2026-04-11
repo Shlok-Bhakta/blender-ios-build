16cab3d1036e9f3096ce7308bb4afae3d42895e0
Shlok Bhakta <shlokbhakta1@gmail.com>
2026-04-11 15:42:00 -0500

    Fix Python Apple cross build arg

--- build_files/build_environment/cmake/python.cmake
+++ build_files/build_environment/cmake/python.cmake
@@ -173,7 +173,7 @@ ${LIBDIR}/ssl/lib64/pkgconfig:${LIBDIR}/lzma/lib/pkgconfig:${LIBDIR}/zlib/share/
     )
 
     if(WITH_APPLE_CROSSPLATFORM)
-      list(APPEND PYTHON_CONFIGURE_EXTRA_ARGS --build=$(./config.guess))
+      list(APPEND PYTHON_CONFIGURE_EXTRA_ARGS ${PLATFORM_BUILD_TARGET})
       if("${CMAKE_OSX_ARCHITECTURES}" STREQUAL "x86_64")
         list(APPEND PYTHON_CONFIGURE_EXTRA_ARGS --host=x86_64-apple-darwin19.0.0)
       else()
